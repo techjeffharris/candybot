dd63df6ef5bd83c3a5337add65eb7adfdd88b144
Jeff Harris <techjeffharris@gmail.com>
2016-03-31 17:10:56 -0700

    faster training, auto-save

--- candies.js
+++ candies.js
@@ -1,218 +1,267 @@
+"use strict"
 
- function Player() {
+function Player() {
 
- 	function areWeStillResting () {
- 		var waitPattern = /Waiting time : [0-9]{1,3}/;
+  this.training = false
+  this.eatBeforeWorkout = true
+  this.autoSaveDelay = 5 * 60 * 1000
+  this.log = {
+    eating : {
+      meals : 0,
+      candies : 0
+    },
+    training : {
+      destination: '',
+      workOuts : 0,
+      startHP : 0,
+      endHP: 0
+    }
+  };
 
- 		var found = 
- 			document.getElementById('mood')
- 				.innerHTML
- 				.match(waitPattern);
+  let self = this
 
- 		if (found) {
-	 		// return the number of seconds we're waiting
-	 		return new Number(found[0]
-	 				.split(' ')[3]
-			) || 0;
- 		} else {
- 			return 0;
- 		}
+  let checkQuestingInterval
+  let scheduleWorkoutTimeout
+  let autosaveInterval
 
- 	};
+  function getTiredTime () {
+    var waitPattern = /Waiting time : [0-9]{1,3}/;
 
-	function getCandiesEaten () {
+    var found = 
+      document.getElementById('mood')
+        .innerHTML
+        .match(waitPattern);
 
-		/*
-		 * get the number of candies eaten by getting the candies_eaten div, 
-		 * spliting it by spaces, getting the fourth chunk which has the number 
-		 * of candies eaten, then ultimately converting that to a number type
-		 */
-		return new Number(
-			document.getElementById('candies_eaten')
-				.innerHTML
-				.split(' ')[3]
-		);
+    if (found) {
+      // return the number of seconds we're waiting
+      return new Number(found[0]
+          .split(' ')[3]
+      ) || 0;
+    } else {
+      return 0;
+    }
 
-	};
+  }
 
-	function getCandiesFound () {
+  function getCandiesEaten () {
 
-		var candiesFoundPattern = /You found [0-9]+/;
+    /*
+     * get the number of candies eaten by getting the candies_eaten div, 
+     * spliting it by spaces, getting the fourth chunk which has the number 
+     * of candies eaten, then ultimately converting that to a number type
+     */
+    return new Number(
+      document.getElementById('candies_eaten')
+        .innerHTML
+        .split(' ')[3]
+    );
 
-		/* 
-		 * get the quest div innerHTML, find the line with the number of candies 
-		 * found, split it by spaces, then get the third chunk.
-		 */
-		return new Number(
-			document.getElementById('quest')
-				.innerHTML
-				.match(candiesFoundPattern)[0]
-				.split(' ')[2]
-		) || 0;
+  }
 
-	};
+  function getCandiesFound () {
 
-	function getCandiesNow () {
-		/*
-		 * get the number of candies right now by getting the candies div, 
-		 * spliting it by spaces, getting the third chunk which has the number 
-		 * of candies, then ultimately converting that to a number type
-		 */ 
-		new Number(document.getElementById('candies').innerHTML.split(' ')[2]);
-	};
-	
-	function getDestination () {
+    var candiesFoundPattern = /You found [0-9]+/;
 
-		// the quest_destination select box
-		var destination = document.getElementById('quest_destination');
+    /* 
+     * get the quest div innerHTML, find the line with the number of candies 
+     * found, split it by spaces, then get the third chunk.
+     */
+    return new Number(
+      document.getElementById('quest')
+        .innerHTML
+        .match(candiesFoundPattern)[0]
+        .split(' ')[2]
+    ) || 0;
 
-		// the id of the option that is selected
-		var child = destination.selectedIndex;
-		
-		// return the text of the option
-		return destination.childNodes[child].innerHTML;
-	};
+  }
 
-	function getHP () {
-		// this pattern finds the HP section
-		var HPPattern = /HP : [0-9]+\/[0-9]+/;
+  function getCandiesNow () {
+    /*
+     * get the number of candies right now by getting the candies div, 
+     * spliting it by spaces, getting the third chunk which has the number 
+     * of candies, then ultimately converting that to a number type
+     */ 
+    return candies.nbrOwned;
+  }
+  
+  function getDestination () {
 
-		// return the player's total HP from the match found using HPPattern
-		return document.getElementById('quest').innerHTML.match(HPPattern)[0].split('/')[1];
-	};
+    // the quest_destination select box
+    var destination = document.getElementById('quest_destination');
 
-	this.log = {
-		eating : {
-			meals : 0,
-			candies : 0,
-		},
-		training : {
-			intervalID : 0,
-			destination: '',
-			workOuts : 0,
-			HP : 0
-		}
-	};
+    // the id of the option that is selected
+    var child = destination.selectedIndex;
+    
+    // return the text of the option
+    return destination.childNodes[child].innerHTML;
+  }
 
-	this.stopTraining = function () {
-		clearInterval($Player.log.training.intervalID);
+  function getHP () {
+    let index = quest.getCharacterIndex()
 
-		var log = $Player.log;
+    return quest.things[index].max_hp
+  }
 
-		console.log('That was one heck of a training session!');
-		console.log('You worked out ' + log.training.workOuts + ' times.');
-		console.log('You ate ' + log.eating.candies + ' candies.' );
-		console.log('These candies have raised your HP to ' + log.training.HP );
-	};
+  function scheduleNextWorkout () {
 
-	this.eat = function() {
+    console.log('scheduling next workout...')
 
-		var candiesNow = getCandiesNow();
-		
-		// eat all the candies!! why wait?
-		candies.eat();
+    // check every second until we have finished questing
+    checkQuestingInterval = setInterval(function () {
 
-		// add the number of meals and candies eaten this time to the log
-		$Player.log.eating.meals++;
-		$Player.log.eating.candies += candiesNow;
+      // console.log('quest.weAreQuestingRightNow', quest.weAreQuestingRightNow)
 
-		var total = getCandiesEaten();
+      // once we are done questing
+      if (!quest.weAreQuestingRightNow) {
 
-		// note the log in the console, no?
-		var msg = new Date() + ': eating ' + candiesNow + ' candies!! ' 
-			+ $Player.log.eating.candies + ' in ' + $Player.log.eating.meals + ' meals; ' 
-			+ total + ' this game.';
+        console.log('we are are not questing!  lets see how long we need to wait until we can go...')
 
-		console.log(msg);
-	};
+        // stop checking every second
+        clearInterval(checkQuestingInterval);
+        checkQuestingInterval = null;
 
-	this.workOut = function () {
+        let tiredTime = getTiredTime()
 
-		var TIMEOUT = 5 * 1000;
+        // now schedule the next quest to happen once tired time has elapsed
+        scheduleWorkoutTimeout = setTimeout(function () {
+          scheduleWorkoutTimeout = null
+          if (self.eatBeforeWorkout) {
+            self.eat()
+          }
+          self.workOut()
+        }, tiredTime * 1000)
+          
+        // output wait time
+        console.log(new Date() + ': workout session in ' + tiredTime + ' seconds...');
 
-		// output 5 second notice
-		console.log(new Date() + ': workout session in ' + TIMEOUT/1000 + ' seconds...');
 
-		// schedule the workout 
-		setTimeout(function() {
-			
-			quest.begin(true);
+    
+      } 
+    }, 1000)
+      
+  }
 
-			// increment the training workout log
-			$Player.log.training.workOuts++;
+  this.stopTraining = function () {
+    self.training = false
 
-			// note the training destination
-			$Player.log.training.destination = getDestination();
+    if (checkQuestingInterval) {
+      clearInterval(checkQuestingInterval)
+    }
 
-			$Player.log.training.HP = getHP();
+    if (scheduleWorkoutTimeout) {
+      clearTimeout(scheduleWorkoutTimeout)
+    }
 
-			// build the console log message
-			var msg = new Date() + ": workout session " + $Player.log.training.workOuts + " at " + $Player.log.training.destination + ' - HP: ' + $Player.log.training.HP;
-			console.log(msg);
 
-		}, TIMEOUT);
-	};
+    var log = self.log;
 
-	this.train = function (options) {
+    console.log('That was one heck of a training session!');
+    console.log('You worked out ' + log.training.workOuts + ' times.');
+    console.log('You ate ' + log.eating.candies + ' candies.' );
+    console.log('These candies have raised your HP from ' + log.training.startHP + ' to ' + log.training.HP + '!' );
 
-		options = options || {};
+    self.log.training.startHP = 0;
+  };
 
-		var eat = 	options.eat 		|| true,
-		interval = 	options.interval 	|| 5 * 60;
+  this.eat = function() {
 
-		// build the confirmation message string
-		var msg = "You're about to train every ";
+    var candiesNow = getCandiesNow();
+    
+    // eat all the candies!! why wait?
+    candies.eat();
 
-		// if interval is longer than 60 seconds
-		if (interval > 60)  {
-			// say how many minutes
-			var trainingInterval = interval/60 + " minutes";
-		} else {
-			// say how many seconds
-			var trainingInterval = interval + " seconds"; 
-		}
+    // add the number of meals and candies eaten this time to the log
+    self.log.eating.meals++;
+    self.log.eating.candies += candiesNow;
 
-		msg += trainingInterval + " starting at " + getDestination() + ".\n\nDo it?";
+    var total = getCandiesEaten();
 
-		// confirm sure they want to train in the selected destination 
-		if (window.confirm(msg)) {
-			// announce the new training schedule
-			console.log(new Date() + ': beginning training at ' + getDestination() + ' every ' + trainingInterval);
+    // note the log in the console, no?
+    var msg = new Date() + ': eating ' + candiesNow + ' candies!! ' 
+      + self.log.eating.candies + ' in ' + self.log.eating.meals + ' meals; ' 
+      + total + ' this game.';
 
-			// do it once now
-			$Player.workOut();
+    console.log(msg);
+  };
 
-			// schedule the interval and save the id for stopping later
-			$Player.log.training.intervalID = setInterval(function trainingSession() {
+  this.workOut = function () {
 
-				// how long we're waiting to heal
-				var waiting = areWeStillResting();
+    let questing = quest.weAreQuestingRightNow
+    let tiredTime = getTiredTime();
 
-				// if we aren't still on a quest and aren't waiting to heal
-				if (document.getElementById('quest').innerHTML === '' && !waiting ) {
-					// if we're supposed to 
-					if (options.eat) {
-						// eat all the candies then begin the quest!
-						$Player.eat();
-					}
+    // if we are questing 
+    if (questing || tiredTime > 0 ) {
+      console.log('we are not ready to workOut')
+      return false 
+    } else {
+      // we are ready to start questing! 
+      console.log('we are ready!  lets workOut!')
+      quest.begin(true);
 
-					$Player.workOut();
-				} else {
+      // increment the training workout log
+      self.log.training.workOuts++;
 
-					var now = new Date();
+      // note the training destination
+      self.log.training.destination = getDestination();
 
-					console.log(now + ': not ready; next session: ' + new Date(now.getTime() + interval * 1000) );
-					
-				}
-			}, interval * 1000);
-		} else {
-			console.log('You canceled training at ' + getDestination() + ' every ' + interval + ' seconds.');
-		}
+      if (self.log.training.startHP === 0) {
+        self.log.training.startHP = quest.things[quest.getCharacterIndex()].max_hp
+      }
 
-	};
+      self.log.training.HP = getHP();
 
-};
+      // build the console log message
+      var msg = new Date() + ": workout session " + self.log.training.workOuts + " at " + self.log.training.destination + ' - HP: ' + self.log.training.HP;
+      console.log(msg);
 
-$Player = new Player();
+      if (self.training) {
+        scheduleNextWorkout()
+      }
 
-console.log ($Player);
+      return true
+    }
+  };
+
+  this.train = function (options) {
+
+    options = options || {};
+
+    // build the confirmation message string
+    var msg = "You're about to train at " + getDestination() + ".\n\nDo it?";
+
+    // confirm sure they want to train in the selected destination 
+    if (window.confirm(msg)) {
+      // announce the new training schedule
+      console.log(new Date() + ': beginning training at ' + getDestination())
+
+      self.training = true
+      self.eatBeforeWorkout = (options.eat === undefined) ? true : options.eat
+
+      scheduleNextWorkout()
+
+    } else {
+      console.log('Okay, we won\'t train at ' + getDestination());
+    }
+
+  };
+
+  this.enableAutoSave = function (delay) { 
+
+    self.autoSaveDelay (delay === undefined) ? self.autoSaveDelay : delay
+
+    autosaveInterval = setInterval(function () { 
+      save() 
+    }, self.autoSaveDelay)
+
+  }
+
+  this.disableAutoSave = function () {
+    clearInterval(autosaveInterval)
+  }
+
+}
+
+
+player = new Player()
+
+console.log (player);
